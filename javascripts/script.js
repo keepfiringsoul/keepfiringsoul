7dc2e20d7198c45ec5e5adf7e146b6e4c3610008
keepfiringsoul <keepfiringsoul@gmail.com>
2013-11-27 01:57:21 +0900

    next

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,4 +1,20 @@
+var scene;
 
+var init = function() {
+    scene = document.querySelector('#scene');    
+};
+
+window.onload = function() {
+    init();  
+};
+
+window.onmousemove = function(e) {
+    var dx = (window.innerWidth / 2 - e.pageX) * 0.1;
+    var dy = (window.innerHeight / 2 - e.pageY) * 0.1;
+    if(scene) {
+        scene.style['-webkit-transform'] = 'rotateX('+dy +'deg)';/*'rotate3d(' + dx + 'deg,' dy + 'deg,'+ 0)'*/
+    }
+};
 
 
 
@@ -17,7 +33,7 @@ var add_pf = function(){
     };
     
 	for(var i=0; i<item_Num; i++){
-     pf[i].id=i;
+     pf[i].idName="b_"+i;
 	 pf[i].style['width'] = '300px';
 	 pf[i].style['height'] = '500px';
 	 pf[i].style['background-color']="black";

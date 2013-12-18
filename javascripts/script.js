5da63c42b5c4675ca5808ecd8f6d2a2409242bd7
keepfiringsoul <keepfiringsoul@gmail.com>
2013-12-18 19:39:41 +0900

    reset

--- javascripts/script.js
+++ javascripts/script.js
@@ -71,7 +71,7 @@ var init = function() {
 				break;
 			case pf[7] :
 				dp_box.innerHTML = 
-				"<video width=\"720\" height=\"404\" autoplay controls><source src=\"./videos/v_4.mp4\" type=\"video/mp4\"></video>";
+				"<iframe width=\"720\" height=\"404\" src=\"http://www.youtube.com/embed/XQeoontIR_s\" frameborder=\"0\" allowfullscreen></iframe>";
 				t_box.innerHTML = "KMUVCD <br> Motion Graphics1 <br> Experimental Video <hr><br> WATER <br> 5'55\" 1280*720 <br> SANYO XACTI VPC-CA9 waterproof camera test";
 				break;
 			case pf[8] :	
@@ -96,14 +96,14 @@ var init = function() {
 				break;
 			case pf[13] :
 				dp_box.innerHTML = 
-				"<video width=\"720\" height=\"480\" autoplay controls><source src=\"./videos/v_3.mp4\" type=\"video/mp4\"></video>";
-				t_box.innerHTML = "KMUVCD <br> Graduation Work <br> Installation video <hr><br> MY STORY <br> 13'49\" 1280*720 ";
+				"<iframe width=\"720\" height=\"480\" src=\"http://www.youtube.com/embed/XmXhbno4j9E\" frameborder=\"0\" allowfullscreen></iframe>";
+				t_box.innerHTML = "KMUVCD <br> Motion Graphics1 <br> Self-portrait <hr><br> CLOCK <br> 6'14\" 850*480 ";
 				break;
 			case pf[14] :
 				dp_box.innerHTML = 
-				"<video width=\"720\" height=\"404\" autoplay controls><source src=\"./videos/v_2.mp4\" type=\"video/mp4\"></video>";
-				t_box.innerHTML = "KMUVCD <br> Motion Graphics1 <br> Self-portrait <hr><br> CLOCK <br> 6'14\" 850*480 ";
-				break;
+				"<iframe width=\"720\" height=\"404\" src=\"http://www.youtube.com/embed/QbceB70CCZg\" frameborder=\"0\" allowfullscreen></iframe>";
+				t_box.innerHTML = "KMUVCD <br> Graduation Work <br> Installation video <hr><br> MY STORY <br> 13'49\" 1280*720 ";
+				break;				
 			case pf[15] :	
 				dp_box.innerHTML = "<img src=\"./images/15.png\">";
 				t_box.innerHTML = "2011 <br> Flee Market Project <br> Stationary design <hr><br> DIARY, MEMO <br> Used paper <br> Inkjet print, hand-binding";
@@ -126,7 +126,7 @@ var init = function() {
 				break;
 			case pf[20] :
 				dp_box.innerHTML = 
-				"<video width=\"720\" height=\"480\" autoplay controls><source src=\"./videos/v_1.mp4\" type=\"video/mp4\"></video>";
+				"<iframe width=\"720\" height=\"480\" src=\"http://www.youtube.com/embed/_FTBZ4ouvLk\" frameborder=\"0\" allowfullscreen></iframe>";
 				t_box.innerHTML = "KMUVCD <br> Motion Graphics2 <br> Self-portrait <hr><br> DEAD TREE, LIVE TREE <br> 6'46\" 720*480 ";
 				break;
 			case pf[21] :	
@@ -171,12 +171,19 @@ var init = function() {
 	    d.style['background-image']='url(./images/'+i+'.png)';     	
 		d.style['-webkit-transform'] = 'translate3d('+randomX+'px,'+randomY+'px,'+randomZ+'px) rotateY(0deg)';		
     	scene.appendChild(d);
-		
+		d.rd_x = randomX;
+		d.rd_y = randomY;
+		d.rd_z = randomZ;
+		if(d.rd_z>0 && d.rd_z<50){
+			d.style['-webkit-filter'] = 'blur(3px)';
+		}
+		if(d.rd_z>50 && d.rd_z<100){
+			d.style['-webkit-filter'] = 'blur(1px)';
+		}
+		if(d.rd_z>100 && d.rd_z<150){
+			d.style['-webkit-filter'] = 'blur(0.5px)';
+		}
 		pf.push(d);
-		rd_x.push(randomX);
-		rd_y.push(randomY);
-		rd_z.push(randomZ);
-		
 		d.onclick= function() {
 			contentsFill(this);
 			screen.style['display'] = 'block';
@@ -190,32 +197,33 @@ var init = function() {
 				t_box.style['opacity'] = '1';
 			},50);		
 			};
-					
-    	}
 		
-		//add_blur
-		for(var i=0;i<item_Num; i++){
-			if(rd_z[i]>0 && rd_z[i]<50){
-			pf[i].style['-webkit-filter'] = 'blur(3px)';
-			}
-			if(rd_z[i]>50 && rd_z[i]<100){
-			pf[i].style['-webkit-filter'] = 'blur(1px)';
-			}
-			if(rd_z[i]>100 && rd_z[i]<150){
-			pf[i].style['-webkit-filter'] = 'blur(0.5px)';
-			}
-		}//add_blur
-		
+		d.onmouseover = function(e){ 
+			//var s = 'translate3d('+rd_x[i]+'px,'+rd_y[i]+'px,'+rd_z[i]+'px) rotateY(360deg)';
+			var s = 'translate3d('+this.rd_x+'px,'+ this.rd_y+'px,'+ this.rd_z +'px) rotateY(360deg)';
+			this.style['-webkit-transform'] = s;
+			console.log(this.id);
+			console.log(s);
+		};
+		d.onmouseout = function(e){ 
+			this.style['-webkit-transform'] = 'translate3d('+this.rd_x+'px,'+ this.rd_y+'px,'+ this.rd_z +'px) rotateY(720deg)';
+		};	
+				
+    }
 		/*
 		for(var i=0;i<item_Num; i++){
-			pf[i].onmouseover = function(){ 
-				pf[i].style['-webkit-transform'] = 'translate3d('+rd_x[i]+'px,'+rd_y[i]+'px,'+rd_z[i]+'px) rotateY(360deg)';
+			
+			pf[i].onmouseover = function(e){ 
+				e.target.style['-webkit-transform'] = 'translate3d('+rd_x[i]+'px,'+rd_y[i]+'px,'+rd_z[i]+'px) rotateY(360deg)';
 				};
-			pf[i].onmouseout = function(){ 
-				pf[i].style['-webkit-transform'] = 'translate3d('+rd_x[i]+'px,'+rd_y[i]+'px,'+rd_z[i]+'px) rotateY(0deg)';
+			pf[i].onmouseout = function(e){ 
+				e.target.style['-webkit-transform'] = 'translate3d('+rd_x[i]+'px,'+rd_y[i]+'px,'+rd_z[i]+'px) rotateY(0deg)';
 				};
-		}*/
-		
+		}
+		*/
+		//미친짓 시작
+		var check = false;
+		if(check) {
 		pf[0].onmouseover = function(){ 
 			pf[0].style['-webkit-transform'] = 'translate3d('+rd_x[0]+'px,'+rd_y[0]+'px,'+rd_z[0]+'px) rotateY(360deg)';
 			};
@@ -234,8 +242,152 @@ var init = function() {
 		pf[2].onmouseout = function(){ 
 			pf[2].style['-webkit-transform'] = 'translate3d('+rd_x[2]+'px,'+rd_y[2]+'px,'+rd_z[2]+'px) rotateY(0deg)';
 			};
-		
-	
+		pf[3].onmouseover = function(){ 
+			pf[3].style['-webkit-transform'] = 'translate3d('+rd_x[3]+'px,'+rd_y[3]+'px,'+rd_z[3]+'px) rotateY(360deg)';
+			};
+		pf[3].onmouseout = function(){ 
+			pf[3].style['-webkit-transform'] = 'translate3d('+rd_x[3]+'px,'+rd_y[3]+'px,'+rd_z[3]+'px) rotateY(0deg)';
+			};
+		pf[4].onmouseover = function(){ 
+			pf[4].style['-webkit-transform'] = 'translate3d('+rd_x[4]+'px,'+rd_y[4]+'px,'+rd_z[4]+'px) rotateY(360deg)';
+			};
+		pf[4].onmouseout = function(){ 
+			pf[4].style['-webkit-transform'] = 'translate3d('+rd_x[4]+'px,'+rd_y[4]+'px,'+rd_z[4]+'px) rotateY(0deg)';
+			};
+		pf[5].onmouseover = function(){ 
+			pf[5].style['-webkit-transform'] = 'translate3d('+rd_x[5]+'px,'+rd_y[5]+'px,'+rd_z[5]+'px) rotateY(360deg)';
+			};
+		pf[5].onmouseout = function(){ 
+			pf[5].style['-webkit-transform'] = 'translate3d('+rd_x[5]+'px,'+rd_y[5]+'px,'+rd_z[5]+'px) rotateY(0deg)';
+			};
+		pf[6].onmouseover = function(){ 
+			pf[6].style['-webkit-transform'] = 'translate3d('+rd_x[6]+'px,'+rd_y[6]+'px,'+rd_z[6]+'px) rotateY(360deg)';
+			};
+		pf[6].onmouseout = function(){ 
+			pf[6].style['-webkit-transform'] = 'translate3d('+rd_x[6]+'px,'+rd_y[6]+'px,'+rd_z[6]+'px) rotateY(0deg)';
+			};
+		pf[7].onmouseover = function(){ 
+			pf[7].style['-webkit-transform'] = 'translate3d('+rd_x[7]+'px,'+rd_y[7]+'px,'+rd_z[7]+'px) rotateY(360deg)';
+			};
+		pf[7].onmouseout = function(){ 
+			pf[7].style['-webkit-transform'] = 'translate3d('+rd_x[7]+'px,'+rd_y[7]+'px,'+rd_z[7]+'px) rotateY(0deg)';
+			};
+		pf[8].onmouseover = function(){ 
+			pf[8].style['-webkit-transform'] = 'translate3d('+rd_x[8]+'px,'+rd_y[8]+'px,'+rd_z[8]+'px) rotateY(360deg)';
+			};
+		pf[8].onmouseout = function(){ 
+			pf[8].style['-webkit-transform'] = 'translate3d('+rd_x[8]+'px,'+rd_y[8]+'px,'+rd_z[8]+'px) rotateY(0deg)';
+			};
+		pf[9].onmouseover = function(){ 
+			pf[9].style['-webkit-transform'] = 'translate3d('+rd_x[9]+'px,'+rd_y[9]+'px,'+rd_z[9]+'px) rotateY(360deg)';
+			};
+		pf[9].onmouseout = function(){ 
+			pf[9].style['-webkit-transform'] = 'translate3d('+rd_x[9]+'px,'+rd_y[9]+'px,'+rd_z[9]+'px) rotateY(0deg)';
+			};
+		pf[10].onmouseover = function(){ 
+			pf[10].style['-webkit-transform'] = 'translate3d('+rd_x[10]+'px,'+rd_y[10]+'px,'+rd_z[10]+'px) rotateY(360deg)';
+			};
+		pf[10].onmouseout = function(){ 
+			pf[10].style['-webkit-transform'] = 'translate3d('+rd_x[10]+'px,'+rd_y[10]+'px,'+rd_z[10]+'px) rotateY(0deg)';
+			};
+		pf[11].onmouseover = function(){ 
+			pf[11].style['-webkit-transform'] = 'translate3d('+rd_x[11]+'px,'+rd_y[11]+'px,'+rd_z[11]+'px) rotateY(360deg)';
+			};
+		pf[11].onmouseout = function(){ 
+			pf[11].style['-webkit-transform'] = 'translate3d('+rd_x[11]+'px,'+rd_y[11]+'px,'+rd_z[11]+'px) rotateY(0deg)';
+			};
+		pf[12].onmouseover = function(){ 
+			pf[12].style['-webkit-transform'] = 'translate3d('+rd_x[12]+'px,'+rd_y[12]+'px,'+rd_z[12]+'px) rotateY(360deg)';
+			};
+		pf[12].onmouseout = function(){ 
+			pf[12].style['-webkit-transform'] = 'translate3d('+rd_x[12]+'px,'+rd_y[12]+'px,'+rd_z[12]+'px) rotateY(0deg)';
+			};
+		pf[13].onmouseover = function(){ 
+			pf[13].style['-webkit-transform'] = 'translate3d('+rd_x[13]+'px,'+rd_y[13]+'px,'+rd_z[13]+'px) rotateY(360deg)';
+			};
+		pf[13].onmouseout = function(){ 
+			pf[13].style['-webkit-transform'] = 'translate3d('+rd_x[13]+'px,'+rd_y[13]+'px,'+rd_z[13]+'px) rotateY(0deg)';
+			};
+		pf[14].onmouseover = function(){ 
+			pf[14].style['-webkit-transform'] = 'translate3d('+rd_x[14]+'px,'+rd_y[14]+'px,'+rd_z[14]+'px) rotateY(360deg)';
+			};
+		pf[14].onmouseout = function(){ 
+			pf[14].style['-webkit-transform'] = 'translate3d('+rd_x[14]+'px,'+rd_y[14]+'px,'+rd_z[14]+'px) rotateY(0deg)';
+			};
+		pf[15].onmouseover = function(){ 
+			pf[15].style['-webkit-transform'] = 'translate3d('+rd_x[15]+'px,'+rd_y[15]+'px,'+rd_z[15]+'px) rotateY(360deg)';
+			};
+		pf[15].onmouseout = function(){ 
+			pf[15].style['-webkit-transform'] = 'translate3d('+rd_x[15]+'px,'+rd_y[15]+'px,'+rd_z[15]+'px) rotateY(0deg)';
+			};
+		pf[16].onmouseover = function(){ 
+			pf[16].style['-webkit-transform'] = 'translate3d('+rd_x[16]+'px,'+rd_y[16]+'px,'+rd_z[16]+'px) rotateY(360deg)';
+			};
+		pf[16].onmouseout = function(){ 
+			pf[16].style['-webkit-transform'] = 'translate3d('+rd_x[16]+'px,'+rd_y[16]+'px,'+rd_z[16]+'px) rotateY(0deg)';
+			};
+		pf[17].onmouseover = function(){ 
+			pf[17].style['-webkit-transform'] = 'translate3d('+rd_x[17]+'px,'+rd_y[17]+'px,'+rd_z[17]+'px) rotateY(360deg)';
+			};
+		pf[17].onmouseout = function(){ 
+			pf[17].style['-webkit-transform'] = 'translate3d('+rd_x[17]+'px,'+rd_y[17]+'px,'+rd_z[17]+'px) rotateY(0deg)';
+			};
+		pf[18].onmouseover = function(){ 
+			pf[18].style['-webkit-transform'] = 'translate3d('+rd_x[18]+'px,'+rd_y[18]+'px,'+rd_z[18]+'px) rotateY(360deg)';
+			};
+		pf[18].onmouseout = function(){ 
+			pf[18].style['-webkit-transform'] = 'translate3d('+rd_x[18]+'px,'+rd_y[18]+'px,'+rd_z[18]+'px) rotateY(0deg)';
+			};
+		pf[19].onmouseover = function(){ 
+			pf[19].style['-webkit-transform'] = 'translate3d('+rd_x[19]+'px,'+rd_y[19]+'px,'+rd_z[19]+'px) rotateY(360deg)';
+			};
+		pf[19].onmouseout = function(){ 
+			pf[19].style['-webkit-transform'] = 'translate3d('+rd_x[19]+'px,'+rd_y[19]+'px,'+rd_z[19]+'px) rotateY(0deg)';
+			};
+		pf[20].onmouseover = function(){ 
+			pf[20].style['-webkit-transform'] = 'translate3d('+rd_x[20]+'px,'+rd_y[20]+'px,'+rd_z[20]+'px) rotateY(360deg)';
+			};
+		pf[20].onmouseout = function(){ 
+			pf[20].style['-webkit-transform'] = 'translate3d('+rd_x[20]+'px,'+rd_y[20]+'px,'+rd_z[20]+'px) rotateY(0deg)';
+			};
+		pf[21].onmouseover = function(){ 
+			pf[21].style['-webkit-transform'] = 'translate3d('+rd_x[21]+'px,'+rd_y[21]+'px,'+rd_z[21]+'px) rotateY(360deg)';
+			};
+		pf[21].onmouseout = function(){ 
+			pf[21].style['-webkit-transform'] = 'translate3d('+rd_x[21]+'px,'+rd_y[21]+'px,'+rd_z[21]+'px) rotateY(0deg)';
+			};
+		pf[22].onmouseover = function(){ 
+			pf[22].style['-webkit-transform'] = 'translate3d('+rd_x[22]+'px,'+rd_y[22]+'px,'+rd_z[22]+'px) rotateY(360deg)';
+			};
+		pf[22].onmouseout = function(){ 
+			pf[22].style['-webkit-transform'] = 'translate3d('+rd_x[22]+'px,'+rd_y[22]+'px,'+rd_z[22]+'px) rotateY(0deg)';
+			};
+		pf[23].onmouseover = function(){ 
+			pf[23].style['-webkit-transform'] = 'translate3d('+rd_x[23]+'px,'+rd_y[23]+'px,'+rd_z[23]+'px) rotateY(360deg)';
+			};
+		pf[23].onmouseout = function(){ 
+			pf[23].style['-webkit-transform'] = 'translate3d('+rd_x[23]+'px,'+rd_y[23]+'px,'+rd_z[23]+'px) rotateY(0deg)';
+			};
+		pf[24].onmouseover = function(){ 
+			pf[24].style['-webkit-transform'] = 'translate3d('+rd_x[24]+'px,'+rd_y[24]+'px,'+rd_z[24]+'px) rotateY(360deg)';
+			};
+		pf[24].onmouseout = function(){ 
+			pf[24].style['-webkit-transform'] = 'translate3d('+rd_x[24]+'px,'+rd_y[24]+'px,'+rd_z[24]+'px) rotateY(0deg)';
+			};
+		pf[25].onmouseover = function(){ 
+			pf[25].style['-webkit-transform'] = 'translate3d('+rd_x[25]+'px,'+rd_y[25]+'px,'+rd_z[25]+'px) rotateY(360deg)';
+			};
+		pf[25].onmouseout = function(){ 
+			pf[25].style['-webkit-transform'] = 'translate3d('+rd_x[25]+'px,'+rd_y[25]+'px,'+rd_z[25]+'px) rotateY(0deg)';
+			};
+		pf[26].onmouseover = function(){ 
+			pf[26].style['-webkit-transform'] = 'translate3d('+rd_x[26]+'px,'+rd_y[26]+'px,'+rd_z[26]+'px) rotateY(360deg)';
+			};
+		pf[26].onmouseout = function(){ 
+			pf[26].style['-webkit-transform'] = 'translate3d('+rd_x[26]+'px,'+rd_y[26]+'px,'+rd_z[26]+'px) rotateY(0deg)';
+			};
+		//미친짓 끝
+		}
 };
 
 window.onload = function() {
